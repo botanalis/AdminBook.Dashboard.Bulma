72e67a5b151b7342f8d2e5d4189e4b60054db9cd
ChingFeng <hcforsch@gmail.com>
2020-12-19 19:05:58 +0800

    Set Project env

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import store from './store';
+import router from './router';
+
 import '../node_modules/bulma/css/bulma.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,
+  router
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+const vuex = new Vuex.Store({
+    strict: false,
+    modules: {},
+    state: {},
+    getters: {},
+    mutations: {},
+    actions:{}
+});
+
+export default vuex;
