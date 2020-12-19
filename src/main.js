132506598fe37c12ce63a45216c899509fca09b6
ChingFeng <hcforsch@gmail.com>
2020-12-19 19:13:00 +0800

    [project]新增fortawesome套件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import App from './App.vue'
 import store from './store';
 import router from './router';
 
+//圖示Icon
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faUserSecret);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 import '../node_modules/bulma/css/bulma.css'
 
 Vue.config.productionTip = false
